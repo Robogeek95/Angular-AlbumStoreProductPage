01318a73111a66886ec5b2f8f5d471fe0ce59146
Robogiek <azeezlukman95@gmail.com>
2019-10-31 20:35:30 +0100

    adding an albumUrl property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
-import { AlbumStorePage } from '../../e2e/app.po';
 
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = '../assets/AlbumStorePage.json'
+  private _albumUrl = '../assets/album.json'
 
   constructor(private _http:Http) { }
 
