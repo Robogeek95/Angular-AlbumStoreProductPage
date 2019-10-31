3b69ec8cbbd55609f4a1e8194947c064620054ca
Robogiek <azeezlukman95@gmail.com>
2019-10-31 20:30:01 +0100

    creating a service

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Responce } from '@angular/http';
+import { AlbumStorePage } from '../../e2e/app.po';
+
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/AlbumStorePage.json'
+
+  constructor(private _http:Http) { }
+
+  getAlbum(id: number) {
+   return this._http.get(this._albumUrl)
+  }
+}
